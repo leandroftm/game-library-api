4518612ad487e386cb81458aed420742c29123e6
Leandro Torres <leandroftm@gmail.com>
2026-05-25 00:01:17 -0300

    feat: created UserGame entity

--- src/main/java/com/leandroftm/game_library_api/domain/entity/UserGame.java
+++ src/main/java/com/leandroftm/game_library_api/domain/entity/UserGame.java
@@ -1,4 +1,79 @@
 package com.leandroftm.game_library_api.domain.entity;
 
+import com.leandroftm.game_library_api.domain.enums.GameStatus;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "user_game")
 public class UserGame {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(nullable = false)
+    private Long igdbId;
+    @Column(nullable = false)
+    private String gameName;
+    @Column(nullable = false)
+    private boolean favorite;
+    @Column(nullable = false)
+    private String platformName;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime startDate;
+    private LocalDateTime completedDate;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private GameStatus status;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    User user;
+
+    public UserGame(Long igdbId, String gameName, boolean favorite, String platformName) {
+        this.igdbId = igdbId;
+        this.gameName = gameName;
+        this.favorite = favorite;
+        this.platformName = platformName;
+        this.status = GameStatus.TO_PLAY;
+    }
+
+    public void associateUser(User user) {
+        this.user = user;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void startPlaying() {
+        if (this.status == GameStatus.PLAYING)
+            throw new IllegalStateException("Game is already playing");
+        if (this.startDate == null)
+            this.startDate = LocalDateTime.now();
+        this.status = GameStatus.PLAYING;
+        this.completedDate = null;
+    }
+
+    public void dropGame() {
+        if (this.status == GameStatus.DROPPED)
+            throw new IllegalStateException("Game is already dropped");
+        this.status = GameStatus.DROPPED;
+        this.startDate = null;
+        this.completedDate = null;
+    }
+
+    public void completeGame() {
+        if (this.status != GameStatus.PLAYING)
+            throw new IllegalStateException("Only playing games can be completed");
+        this.status = GameStatus.COMPLETED;
+        this.completedDate = LocalDateTime.now();
+    }
 }
