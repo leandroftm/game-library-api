c1a8a9b808d3ef57bbd13e5abf0ea5874d4a9f40
Leandro Torres <leandroftm@gmail.com>
2026-05-24 22:37:35 -0300

    first commit: create project

---
+++ src/main/java/com/leandroftm/game_library_api/GameLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.leandroftm.game_library_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandroftm/game_library_api/GameLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandroftm.game_library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
