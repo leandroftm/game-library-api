8c86ba51592e5e21f2c05839b9643340db83183c
Leandro Torres <leandroftm@gmail.com>
2026-05-27 22:51:32 -0300

    feat: add properties class for IGDB

--- src/main/java/com/leandroftm/game_library_api/integration/igdb/config/IgdbConfig.java
+++ src/main/java/com/leandroftm/game_library_api/integration/igdb/config/IgdbConfig.java
@@ -1,4 +1,20 @@
 package com.leandroftm.game_library_api.integration.igdb.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
 public class IgdbConfig {
+
+    @Bean
+    public RestClient igdbRestClient(
+            IgdbProperties properties
+    ) {
+        return RestClient.builder()
+                .baseUrl(properties.getBaseUrl())
+                .defaultHeader("Client-ID", properties.getClientId())
+                .defaultHeader("Authorization", "Bearer " + properties.getToken())
+                .build();
+    }
 }
--- src/main/java/com/leandroftm/game_library_api/integration/igdb/config/IgdbProperties.java
+++ src/main/java/com/leandroftm/game_library_api/integration/igdb/config/IgdbProperties.java
@@ -1,8 +1,12 @@
-package com.leandroftm.game_library_api.integration.igdb;
+package com.leandroftm.game_library_api.integration.igdb.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "igdb")
+@Getter
+@Setter
 public class IgdbProperties {
     private String baseUrl;
     private String clientId;
