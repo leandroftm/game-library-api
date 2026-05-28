5c6397a9c6a68d1b817223f5803c269c6664ff57
Leandro Torres <leandroftm@gmail.com>
2026-05-27 22:52:24 -0300

    feat: add IGDB Rest CLient

--- src/main/java/com/leandroftm/game_library_api/integration/igdb/client/IgdbClient.java
+++ src/main/java/com/leandroftm/game_library_api/integration/igdb/client/IgdbClient.java
@@ -1,4 +1,44 @@
 package com.leandroftm.game_library_api.integration.igdb.client;
 
+import com.leandroftm.game_library_api.integration.igdb.dto.IgdbGameResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class IgdbClient {
+    private final RestClient restClient;
+
+    public List<IgdbGameResponse> searchGames(String gameName) {
+        String body = """
+                search "%s;
+                fields name, platforms.name;
+                limit 10;
+                """.formatted(gameName);
+
+        return restClient.post()
+                .uri("/games")
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+    }
+
+    public String searchGame(String gameName) {
+
+        String body = """
+                search "%s";
+                fields name;
+                limit 10;
+                """.formatted(gameName);
+
+        return restClient.post()
+                .uri("/games")
+                .body(body)
+                .retrieve()
+                .body(String.class);
+    }
 }
