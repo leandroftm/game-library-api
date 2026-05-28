5c3e98c0cfc7da6ec83706f908045afe5542472b
Leandro Torres <leandroftm@gmail.com>
2026-05-27 22:52:51 -0300

    feat: add API connection test

--- src/main/java/com/leandroftm/game_library_api/integration/igdb/TestIGDB.java
+++ src/main/java/com/leandroftm/game_library_api/integration/igdb/TestIGDB.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Test {
+public class TestIGDB {
 
     @Bean
     CommandLineRunner test(IgdbClient igdbClient) {
