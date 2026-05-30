14160ff3444c85615c21bc3cc633529f03d96977
Leandro Torres <leandroftm@gmail.com>
2026-05-29 23:31:21 -0300

    refactor: add game parameters first release date platforms and genres, add platform and genre responses inner records

--- src/main/java/com/leandroftm/game_library_api/integration/igdb/dto/IgdbGameResponse.java
+++ src/main/java/com/leandroftm/game_library_api/integration/igdb/dto/IgdbGameResponse.java
@@ -1,8 +1,17 @@
 package com.leandroftm.game_library_api.integration.igdb.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public record IgdbGameResponse(
         Long id,
         String name,
-        String platform
+        @JsonProperty("first_release_date")
+        Long firstReleaseDate,
+        List<PlatformResponse> platforms,
+        List<GenreResponse> genres
 ) {
+    public record PlatformResponse(String name) { }
+    public record GenreResponse(String name) { }
 }
