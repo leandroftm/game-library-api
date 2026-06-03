50c1211f1fbd84d022c580328ae7b487b83b35c7
Leandro Torres <leandroftm@gmail.com>
2026-06-03 02:01:20 -0300

    update twitch token

---
+++ src/main/java/com/leandroftm/game_library_api/exception/igdb/IgdbGameNotFoundException.java
@@ -0,0 +1,7 @@
+package com.leandroftm.game_library_api.exception.igdb;
+
+public class IgdbGameNotFoundException extends RuntimeException {
+  public IgdbGameNotFoundException(String message) {
+    super(message);
+  }
+}
