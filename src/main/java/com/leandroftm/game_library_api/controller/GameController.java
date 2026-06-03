977484509fc833864c7f55d5cf32905003667f11
Leandro Torres <leandroftm@gmail.com>
2026-06-03 02:06:31 -0300

    feat: created controller for igdb games, with search games by id or search games by name

--- src/main/java/com/leandroftm/game_library_api/controller/GameController.java
+++ src/main/java/com/leandroftm/game_library_api/controller/GameController.java
@@ -1,4 +1,30 @@
 package com.leandroftm.game_library_api.controller;
 
+import com.leandroftm.game_library_api.domain.dto.GameSearchResponse;
+import com.leandroftm.game_library_api.service.IgdbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/games")
 public class GameController {
+
+    private final IgdbService igdbService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GameSearchResponse> searchGameById(@PathVariable long id) {
+        return ResponseEntity.ok(igdbService.searchGameById(id));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<GameSearchResponse>> searchGame(@RequestParam String name, @PageableDefault(size = 10, sort = "name") Pageable pageable) {
+        Page<GameSearchResponse> page = igdbService.searchGamesByName(name, pageable);
+
+        return ResponseEntity.ok(page);
+    }
 }
