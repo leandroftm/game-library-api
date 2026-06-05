660986e3de728c843730b31af6d8212b6a5a7a9e
Leandro Torres <leandroftm@gmail.com>
2026-06-04 23:21:31 -0300

    feat: created repository actions

--- src/main/java/com/leandroftm/game_library_api/repository/UserRepository.java
+++ src/main/java/com/leandroftm/game_library_api/repository/UserRepository.java
@@ -1,7 +1,21 @@
 package com.leandroftm.game_library_api.repository;
 
+import com.leandroftm.game_library_api.domain.dto.response.UserResponse;
 import com.leandroftm.game_library_api.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUserName(String userName);
+
+    boolean existsByEmail(String email);
+
+    Optional<UserResponse> findByEmail(String email);
+
+    Optional<UserResponse>  findByUserName(String userName);
+
+    boolean existsByIdAndUserNameNot(long id, String userName);
+
+    boolean existsByIdAndEmailNot(long id, String email);
 }
