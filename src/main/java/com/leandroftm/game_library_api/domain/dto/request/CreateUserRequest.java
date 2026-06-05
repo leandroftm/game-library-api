8e0a9ff63187acba078358dbcf1ab8f73bebb761
Leandro Torres <leandroftm@gmail.com>
2026-06-04 23:22:06 -0300

    feat: created user request

--- src/main/java/com/leandroftm/game_library_api/domain/dto/request/CreateUserRequest.java
+++ src/main/java/com/leandroftm/game_library_api/domain/dto/request/CreateUserRequest.java
@@ -1,4 +1,22 @@
 package com.leandroftm.game_library_api.domain.dto.request;
 
-public record CreateUserRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateUserRequest(
+        @NotBlank
+        @Min(5)
+        @Max(25)
+        String userName,
+        @NotBlank
+        @Min(6)
+        @Max(25)
+        String password,
+        @NotBlank
+        @Email
+        @Max(50)
+        String email
+) {
 }
