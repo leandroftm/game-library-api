5520fac554a7f4334fe07026238a5dc97bd7d458
Leandro Torres <leandroftm@gmail.com>
2026-06-04 23:22:43 -0300

    feat: created user service

--- src/main/java/com/leandroftm/game_library_api/service/UserService.java
+++ src/main/java/com/leandroftm/game_library_api/service/UserService.java
@@ -1,4 +1,81 @@
 package com.leandroftm.game_library_api.service;
 
+import com.leandroftm.game_library_api.domain.dto.request.CreateUserRequest;
+import com.leandroftm.game_library_api.domain.dto.response.UserResponse;
+import com.leandroftm.game_library_api.domain.entity.User;
+import com.leandroftm.game_library_api.exception.domain.user.UserEmailAlreadyExistsException;
+import com.leandroftm.game_library_api.exception.domain.user.UserNameAlreadyExistsException;
+import com.leandroftm.game_library_api.exception.domain.user.UserNotFoundException;
+import com.leandroftm.game_library_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void createUser(CreateUserRequest request) {
+        if (userRepository.existsByUserName(request.userName())) {
+            throw new UserNameAlreadyExistsException();
+        }
+
+        if (userRepository.existsByEmail(request.email())) {
+            throw new UserEmailAlreadyExistsException();
+        }
+
+        User user = new User(request.userName(), request.password(), request.email());
+        userRepository.save(user);
+    }
+
+    public UserResponse getUserById(Long id) {
+        return userRepository.findById(id)
+                .map(UserResponse::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    public UserResponse getUserByName(String userName) {
+        return userRepository.findByUserName(userName)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    public UserResponse getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    public void updateUser(long id, String userName, String password, String email) {
+        if(userName != null) {
+            if(userRepository.existsByIdAndUserNameNot(id, userName)){
+                throw new UserNameAlreadyExistsException();
+            }
+        }
+
+        if(email != null) {
+            if(userRepository.existsByIdAndEmailNot(id, email)){
+                throw new UserEmailAlreadyExistsException();
+            }
+        }
+
+        User user = userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+
+        user.updateDetails(userName, password, email);
+        userRepository.save(user);
+    }
+
+    public void enable(long id) {
+        User user =  userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+        user.enable();
+        userRepository.save(user);
+    }
+
+    private void disable(long id) {
+        User user =  userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+        user.disable();
+        userRepository.save(user);
+    }
 }
