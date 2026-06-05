d705f77727dd7dd2e5f6325d7fff57853310cfb6
Leandro Torres <leandroftm@gmail.com>
2026-06-04 23:22:24 -0300

    feat: created user response

--- src/main/java/com/leandroftm/game_library_api/domain/dto/response/UserResponse.java
+++ src/main/java/com/leandroftm/game_library_api/domain/dto/response/UserResponse.java
@@ -1,4 +1,18 @@
 package com.leandroftm.game_library_api.domain.dto.response;
 
-public class UserResponse {
+import com.leandroftm.game_library_api.domain.entity.User;
+import com.leandroftm.game_library_api.domain.entity.UserGame;
+
+import java.util.List;
+
+public record UserResponse(
+        String userName,
+        String password,
+        String email,
+        List<UserGame> games
+) {
+
+    public UserResponse(User user){
+        this(user.getUserName(),user.getPassword(),user.getEmail(),user.getGames());
+    }
 }
