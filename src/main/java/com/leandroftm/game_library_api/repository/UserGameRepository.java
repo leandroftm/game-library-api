6091693bdec7c143662b59f4257b8f147a9e5c07
Leandro Torres <leandroftm@gmail.com>
2026-06-10 00:24:51 -0300

    feat: add find by user game id and user id condition

--- src/main/java/com/leandroftm/game_library_api/repository/UserGameRepository.java
+++ src/main/java/com/leandroftm/game_library_api/repository/UserGameRepository.java
@@ -3,6 +3,10 @@ package com.leandroftm.game_library_api.repository;
 import com.leandroftm.game_library_api.domain.entity.UserGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserGameRepository extends JpaRepository<UserGame, Long> {
     boolean existsByUserIdAndIgdbId(Long userId, Long igdbGameId);
+
+    Optional<UserGame> findByIdAndUserId(Long gameId, Long userId);
 }
