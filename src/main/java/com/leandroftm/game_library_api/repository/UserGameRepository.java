166fdf6f4e84d2777d65d93fc05bbf8fee3fc2b0
Leandro Torres <leandroftm@gmail.com>
2026-06-09 23:30:45 -0300

    feat: created check if game exists by user id and API id

--- src/main/java/com/leandroftm/game_library_api/repository/UserGameRepository.java
+++ src/main/java/com/leandroftm/game_library_api/repository/UserGameRepository.java
@@ -4,4 +4,5 @@ import com.leandroftm.game_library_api.domain.entity.UserGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserGameRepository extends JpaRepository<UserGame, Long> {
+    boolean existsByUserIdAndIgdbId(Long userId, Long igdbGameId);
 }
