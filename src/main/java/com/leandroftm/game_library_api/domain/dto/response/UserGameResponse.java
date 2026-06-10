8cb9b3ed0dfe50a64f0990103536c6e8755b6728
Leandro Torres <leandroftm@gmail.com>
2026-06-09 23:32:43 -0300

    feat: created dto response for user game

--- src/main/java/com/leandroftm/game_library_api/domain/dto/response/UserGameResponse.java
+++ src/main/java/com/leandroftm/game_library_api/domain/dto/response/UserGameResponse.java
@@ -1,4 +1,28 @@
 package com.leandroftm.game_library_api.domain.dto.response;
 
-public record UserGameResponse() {
+import com.leandroftm.game_library_api.domain.entity.UserGame;
+import com.leandroftm.game_library_api.domain.enums.GameStatus;
+
+import java.time.LocalDateTime;
+
+public record UserGameResponse(
+        Long id,
+        Long igdbId,
+        String gameName,
+        boolean favorite,
+        LocalDateTime startDate,
+        LocalDateTime completeDate,
+        GameStatus status
+) {
+    public UserGameResponse(UserGame userGame){
+        this(
+                userGame.getId(),
+                userGame.getIgdbId(),
+                userGame.getGameName(),
+                userGame.isFavorite(),
+                userGame.getStartDate(),
+                userGame.getCompletedDate(),
+                userGame.getStatus()
+        );
+    }
 }
