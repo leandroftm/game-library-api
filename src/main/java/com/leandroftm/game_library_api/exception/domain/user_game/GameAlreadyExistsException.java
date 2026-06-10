24005676875d28c86295fef9cdc1cafad872caf1
Leandro Torres <leandroftm@gmail.com>
2026-06-09 23:28:03 -0300

    chore: add exception for when a game already exists for the user

--- src/main/java/com/leandroftm/game_library_api/exception/domain/user_game/GameAlreadyExistsException.java
+++ src/main/java/com/leandroftm/game_library_api/exception/domain/user_game/GameAlreadyExistsException.java
@@ -1,7 +1,12 @@
 package com.leandroftm.game_library_api.exception.domain.user_game;
 
-public class GameAlreadyExistsException extends RuntimeException {
-  public GameAlreadyExistsException(String message) {
-    super(message);
-  }
+import com.leandroftm.game_library_api.exception.Enum.ErrorCode;
+import com.leandroftm.game_library_api.exception.domain.DataIntegrityViolationException;
+
+public class GameAlreadyExistsException extends DataIntegrityViolationException {
+    public GameAlreadyExistsException() {
+        super(
+                "Game already exists", ErrorCode.GAME_ALREADY_EXISTS
+        );
+    }
 }
