2c13052c0978c9e9a7836bf7d3d3a663f25247f3
Leandro Torres <leandroftm@gmail.com>
2026-06-10 00:25:57 -0300

    feat: add dto update game status request

--- src/main/java/com/leandroftm/game_library_api/domain/dto/request/UpdateGameStatusRequest.java
+++ src/main/java/com/leandroftm/game_library_api/domain/dto/request/UpdateGameStatusRequest.java
@@ -2,7 +2,7 @@ package com.leandroftm.game_library_api.domain.dto.request;
 
 import com.leandroftm.game_library_api.domain.enums.GameStatus;
 
-public record GameStatusRequest(
+public record UpdateGameStatusRequest(
         GameStatus status
 ) {
 }
