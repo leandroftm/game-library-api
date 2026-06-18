927c75fc6e67990ea76628e66c69ed8a45651f24
Leandro Torres <leandroftm@gmail.com>
2026-06-17 22:20:53 -0300

    feat: add jpa specification

--- src/main/java/com/leandroftm/game_library_api/repository/UserGameRepository.java
+++ src/main/java/com/leandroftm/game_library_api/repository/UserGameRepository.java
@@ -2,11 +2,13 @@ package com.leandroftm.game_library_api.repository;
 
 import com.leandroftm.game_library_api.domain.entity.UserGame;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface UserGameRepository extends JpaRepository<UserGame, Long> {
+public interface UserGameRepository extends JpaRepository<UserGame, Long>, JpaSpecificationExecutor<UserGame> {
     boolean existsByUserIdAndIgdbId(Long userId, Long igdbGameId);
 
     Optional<UserGame> findByIdAndUserId(Long gameId, Long userId);
+
 }
