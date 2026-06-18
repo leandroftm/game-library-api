419be264e0376440c02eba9e6af88719f5792bd8
Leandro Torres <leandroftm@gmail.com>
2026-06-17 22:26:18 -0300

    feat: add param filters for specification on /games get mapping

--- src/main/java/com/leandroftm/game_library_api/controller/UserController.java
+++ src/main/java/com/leandroftm/game_library_api/controller/UserController.java
@@ -3,7 +3,9 @@ package com.leandroftm.game_library_api.controller;
 import com.leandroftm.game_library_api.domain.dto.request.AddGameRequest;
 import com.leandroftm.game_library_api.domain.dto.request.CreateUserRequest;
 import com.leandroftm.game_library_api.domain.dto.request.UpdateGameStatusRequest;
+import com.leandroftm.game_library_api.domain.dto.response.UserGameResponse;
 import com.leandroftm.game_library_api.domain.dto.response.UserResponse;
+import com.leandroftm.game_library_api.domain.enums.GameStatus;
 import com.leandroftm.game_library_api.service.UserGameService;
 import com.leandroftm.game_library_api.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -31,9 +33,21 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<UserResponse>> getAllUsers(@PageableDefault(size = 20, sort = "userName") Pageable pageable){
-        Page<UserResponse> page =  userService.getUsers(pageable);
+    public ResponseEntity<Page<UserResponse>> getAllUsers(@PageableDefault(size = 20, sort = "userName") Pageable pageable) {
+        Page<UserResponse> page = userService.getUsers(pageable);
 
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{userId}/games")
+    public ResponseEntity<Page<UserGameResponse>> getAllGamesFromUserByFilter(
+            @PathVariable Long userId,
+            @RequestParam(required = false) GameStatus status,
+            @RequestParam(required = false) Boolean favorite,
+            @RequestParam(required = false) String gameName,
+            @PageableDefault(size = 20, sort = "gameName") Pageable pageable) {
+
+        Page<UserGameResponse> page = userGameService.getGamesByUser(userId, status, favorite, gameName, pageable);
         return ResponseEntity.ok(page);
     }
 
@@ -45,21 +59,27 @@ public class UserController {
     }
 
     @PatchMapping("/{userId}/games/{gameId}/status")
-    public ResponseEntity<Void> changeGameStatus(@PathVariable Long userId, @PathVariable Long gameId, @RequestBody UpdateGameStatusRequest request){
+    public ResponseEntity<Void> changeGameStatus(@PathVariable Long userId, @PathVariable Long gameId, @RequestBody UpdateGameStatusRequest request) {
         userGameService.updateGameStatus(userId, gameId, request);
 
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{userId}/games/{gameId}/favorite")
-    public ResponseEntity<Void> favoriteGame(@PathVariable Long userId, @PathVariable Long gameId){
+    public ResponseEntity<Void> favoriteGame(@PathVariable Long userId, @PathVariable Long gameId) {
         userGameService.favoriteGame(userId, gameId);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{userId}/games/{gameId}/unfavorite")
-    public ResponseEntity<Void> unfavoriteGame(@PathVariable Long userId, @PathVariable Long gameId){
+    public ResponseEntity<Void> unfavoriteGame(@PathVariable Long userId, @PathVariable Long gameId) {
         userGameService.unfavoriteGame(userId, gameId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{userId}/games/{gameId}")
+    public ResponseEntity<Void> deleteGame(@PathVariable Long userId, @PathVariable Long gameId) {
+        userGameService.deleteGame(userId, gameId);
         return ResponseEntity.noContent().build();
     }
 }
