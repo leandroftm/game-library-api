3d14213e4872ac95291d91c1e569ed7484c9c058
Leandro Torres <leandroftm@gmail.com>
2026-07-08 01:27:42 -0300

    feat: add exception for password conflict

--- src/main/java/com/leandroftm/game_library_api/exception/domain/user/PasswordConflictException.java
+++ src/main/java/com/leandroftm/game_library_api/exception/domain/user/PasswordConflictException.java
@@ -1,7 +1,12 @@
 package com.leandroftm.game_library_api.exception.domain.user;
 
-public class PasswordConflictException extends RuntimeException {
-  public PasswordConflictException(String message) {
-    super(message);
-  }
+import com.leandroftm.game_library_api.exception.Enum.ErrorCode;
+import com.leandroftm.game_library_api.exception.domain.DataIntegrityViolationException;
+
+public class PasswordConflictException extends DataIntegrityViolationException {
+    public PasswordConflictException() {
+        super(
+                "The saved password and the new password are the same", ErrorCode.USER_PASSWORD_CONFLICT
+        );
+    }
 }
