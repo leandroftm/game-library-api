d33e183f48be28b7c0e60aab748d6a0b13df4b7c
Leandro Torres <leandroftm@gmail.com>
2026-07-08 01:33:28 -0300

    feat: add update request dto for user

--- src/main/java/com/leandroftm/game_library_api/domain/dto/request/UpdateUserRequest.java
+++ src/main/java/com/leandroftm/game_library_api/domain/dto/request/UpdateUserRequest.java
@@ -1,4 +1,15 @@
 package com.leandroftm.game_library_api.domain.dto.request;
 
-public record UpdateUserRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public record UpdateUserRequest(
+        @Size(min = 5, max = 50)
+        String userName,
+        @Size(min = 6, max = 25)
+        String password,
+        @Email
+        @Size(min = 6, max = 100)
+        String email
+) {
 }
