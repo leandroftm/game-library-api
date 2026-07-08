81ff22a4e56e08e3a0035ee02ea6a8d4d6831930
Leandro Torres <leandroftm@gmail.com>
2026-07-08 01:50:40 -0300

    feat: add authentication requirement, create user previously moved to auth controller, get all users endpoint removed

--- src/main/java/com/leandroftm/game_library_api/controller/UserController.java
+++ src/main/java/com/leandroftm/game_library_api/controller/UserController.java
@@ -1,85 +1,83 @@
 package com.leandroftm.game_library_api.controller;
 
 import com.leandroftm.game_library_api.domain.dto.request.AddGameRequest;
-import com.leandroftm.game_library_api.domain.dto.request.CreateUserRequest;
 import com.leandroftm.game_library_api.domain.dto.request.UpdateGameStatusRequest;
 import com.leandroftm.game_library_api.domain.dto.response.UserGameResponse;
 import com.leandroftm.game_library_api.domain.dto.response.UserResponse;
 import com.leandroftm.game_library_api.domain.enums.GameStatus;
+import com.leandroftm.game_library_api.security.entity.UserPrincipal;
 import com.leandroftm.game_library_api.service.UserGameService;
 import com.leandroftm.game_library_api.service.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
-public class UserController {
+@SecurityRequirement(name = "bearerAuth")
+public class UserController{
 
-    private final UserService userService;
+    //private final UserService userService;
 
     private final UserGameService userGameService;
 
-    @PostMapping
-    public ResponseEntity<Void> createUser(@RequestBody CreateUserRequest request) {
-        userService.createUser(request);
+    //CREATE user moved to auth
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
+    //Test purpose only
+//    @GetMapping("/test/getallusers")
+//    public ResponseEntity<Page<UserResponse>> getAllUsers(@PageableDefault(size = 20, sort = "userName") Pageable pageable) {
+//        Page<UserResponse> page = userService.getUsers(pageable);
+//
+//        return ResponseEntity.ok(page);
+//    }
 
-    @GetMapping
-    public ResponseEntity<Page<UserResponse>> getAllUsers(@PageableDefault(size = 20, sort = "userName") Pageable pageable) {
-        Page<UserResponse> page = userService.getUsers(pageable);
-
-        return ResponseEntity.ok(page);
-    }
-
-    @GetMapping("/{userId}/games")
+    @GetMapping("/me/games")
     public ResponseEntity<Page<UserGameResponse>> getAllGamesFromUserByFilter(
-            @PathVariable Long userId,
+            @AuthenticationPrincipal UserPrincipal user,
             @RequestParam(required = false) GameStatus status,
             @RequestParam(required = false) Boolean favorite,
             @RequestParam(required = false) String gameName,
             @PageableDefault(size = 20, sort = "gameName") Pageable pageable) {
 
-        Page<UserGameResponse> page = userGameService.getGamesByUser(userId, status, favorite, gameName, pageable);
+        Page<UserGameResponse> page = userGameService.getGamesByUser(user.getId(), status, favorite, gameName, pageable);
         return ResponseEntity.ok(page);
     }
 
-    @PostMapping("/{userId}/games")
-    public ResponseEntity<Void> addGame(@PathVariable Long userId, @RequestBody AddGameRequest request) {
-        userGameService.addGame(userId, request);
+    @PostMapping("/me/games")
+    public ResponseEntity<Void> addGame(@AuthenticationPrincipal UserPrincipal user, @RequestBody AddGameRequest request) {
+        userGameService.addGame(user.getId(), request);
 
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/{userId}/games/{gameId}/status")
-    public ResponseEntity<Void> changeGameStatus(@PathVariable Long userId, @PathVariable Long gameId, @RequestBody UpdateGameStatusRequest request) {
-        userGameService.updateGameStatus(userId, gameId, request);
+    @PatchMapping("/me/games/{gameId}/status")
+    public ResponseEntity<Void> changeGameStatus(@AuthenticationPrincipal UserPrincipal user, @PathVariable Long gameId, @RequestBody UpdateGameStatusRequest request) {
+        userGameService.updateGameStatus(user.getId(), gameId, request);
 
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/{userId}/games/{gameId}/favorite")
-    public ResponseEntity<Void> favoriteGame(@PathVariable Long userId, @PathVariable Long gameId) {
-        userGameService.favoriteGame(userId, gameId);
+    @PatchMapping("/me/games/{gameId}/favorite")
+    public ResponseEntity<Void> favoriteGame(@AuthenticationPrincipal UserPrincipal user, @PathVariable Long gameId) {
+        userGameService.favoriteGame(user.getId(), gameId);
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/{userId}/games/{gameId}/unfavorite")
-    public ResponseEntity<Void> unfavoriteGame(@PathVariable Long userId, @PathVariable Long gameId) {
-        userGameService.unfavoriteGame(userId, gameId);
+    @PatchMapping("/me/games/{gameId}/unfavorite")
+    public ResponseEntity<Void> unfavoriteGame(@AuthenticationPrincipal UserPrincipal user, @PathVariable Long gameId) {
+        userGameService.unfavoriteGame(user.getId(), gameId);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/{userId}/games/{gameId}")
-    public ResponseEntity<Void> deleteGame(@PathVariable Long userId, @PathVariable Long gameId) {
-        userGameService.deleteGame(userId, gameId);
+    @DeleteMapping("/me/games/{gameId}")
+    public ResponseEntity<Void> deleteGame(@AuthenticationPrincipal UserPrincipal user, @PathVariable Long gameId) {
+        userGameService.deleteGame(user.getId(), gameId);
         return ResponseEntity.noContent().build();
     }
 }
