19028ad32aa6c15adce23c7dc125911ea191e11a
Leandro Torres <leandroftm@gmail.com>
2026-07-08 01:52:26 -0300

    test: created service tests for user and user game

--- src/test/java/com/leandroftm/game_library_api/service/UserGameServiceTest.java
+++ src/test/java/com/leandroftm/game_library_api/service/UserGameServiceTest.java
@@ -1,4 +1,227 @@
 package com.leandroftm.game_library_api.service;
 
+import com.leandroftm.game_library_api.domain.dto.request.AddGameRequest;
+import com.leandroftm.game_library_api.domain.dto.request.UpdateGameStatusRequest;
+import com.leandroftm.game_library_api.domain.dto.response.GameSearchResponse;
+import com.leandroftm.game_library_api.domain.dto.response.UserGameResponse;
+import com.leandroftm.game_library_api.domain.entity.User;
+import com.leandroftm.game_library_api.domain.entity.UserGame;
+import com.leandroftm.game_library_api.domain.enums.GameStatus;
+import com.leandroftm.game_library_api.exception.domain.user.UserNotFoundException;
+import com.leandroftm.game_library_api.exception.domain.user_game.GameAlreadyExistsException;
+import com.leandroftm.game_library_api.exception.domain.user_game.InvalidGameStatusException;
+import com.leandroftm.game_library_api.repository.UserGameRepository;
+import com.leandroftm.game_library_api.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class UserGameServiceTest {
+
+    @Mock
+    private UserGameRepository userGameRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private IgdbService igdbService;
+
+    @InjectMocks
+    private UserGameService userGameService;
+
+
+    @Test
+    void shouldAddGameSuccessfully() {
+        GameSearchResponse response = new GameSearchResponse(
+                1L,
+                "Fallout",
+                LocalDate.now(),
+                List.of("PC"),
+                List.of("Role Playing Game")
+        );
+
+        when(igdbService.searchGameById(1L)).thenReturn(response);
+        when(userGameRepository.existsByUserIdAndIgdbId(1L, 1L)).thenReturn(false);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(
+                new User(
+                        "UserName",
+                        "encoded-password",
+                        "email@email.com"
+                )));
+
+        userGameService.addGame(1L, new AddGameRequest(1L));
+
+        verify(igdbService).searchGameById(1L);
+        verify(userGameRepository).existsByUserIdAndIgdbId(1L, 1L);
+        verify(userRepository).findById(1L);
+        verify(userGameRepository).save(argThat(game ->
+                game.getIgdbId() == 1L &&
+                        game.getGameName().equals("Fallout")
+        ));
+        verifyNoMoreInteractions(userGameRepository, userRepository, igdbService);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void shouldReturnGamesByUserSuccessfully() {
+        Pageable pageable = PageRequest.of(0, 10);
+        List<UserGame> games = List.of(new UserGame(1L, "Fallout"));
+        Page<UserGame> response = new PageImpl<>(games, pageable, games.size());
+
+        when(userGameRepository.findAll(any(Specification.class), any(Pageable.class))).thenReturn(response);
+
+        Page<UserGameResponse> result = userGameService.getGamesByUser(1L, null, null, null, pageable);
+
+        assertNotNull(result);
+        assertEquals(1L, result.getContent().getFirst().igdbId());
+        assertEquals("Fallout", result.getContent().getFirst().gameName());
+
+        verify(userGameRepository).findAll(any(Specification.class), any(Pageable.class));
+        verifyNoMoreInteractions(userGameRepository);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void shouldReturnGamesByUserAndStatusSuccessfully() {
+        Pageable pageable = PageRequest.of(0, 10);
+        List<UserGame> games = List.of(new UserGame(1L, "Fallout"));
+        ReflectionTestUtils.setField(games.getFirst(), "status", GameStatus.PLAYING);
+
+        Page<UserGame> response = new PageImpl<>(games, pageable, games.size());
+
+        when(userGameRepository.findAll(any(Specification.class), any(Pageable.class))).thenReturn(response);
+
+        Page<UserGameResponse> result = userGameService.getGamesByUser(1L, GameStatus.PLAYING, null, null, pageable);
+
+        assertNotNull(result);
+        assertEquals(1L, games.getFirst().getIgdbId());
+        assertEquals("Fallout", games.getFirst().getGameName());
+        assertEquals(GameStatus.PLAYING, games.getFirst().getStatus());
+
+        verify(userGameRepository).findAll(any(Specification.class), any(Pageable.class));
+        verifyNoMoreInteractions(userGameRepository);
+    }
+
+    @Test
+    void shouldUpdateGameStatusSuccessfully() {
+        UpdateGameStatusRequest request = new UpdateGameStatusRequest(GameStatus.PLAYING);
+        UserGame savedGame = new UserGame(1L, "Fallout");
+        ReflectionTestUtils.setField(savedGame, "status", GameStatus.TO_PLAY);
+
+        when(userGameRepository.findByIdAndUserId(1L, 1L)).thenReturn(Optional.of(savedGame));
+
+        userGameService.updateGameStatus(1L, 1L, request);
+
+        verify(userGameRepository).findByIdAndUserId(1L, 1L);
+        verify(userGameRepository).save(argThat(game ->
+                game.getStatus().equals(GameStatus.PLAYING)));
+        verifyNoMoreInteractions(userGameRepository);
+    }
+
+    @Test
+    void shouldFavoriteGameSuccessfully() {
+        UserGame savedGame = new UserGame(1L, "Fallout");
+
+        when(userGameRepository.findByIdAndUserId(1L, 1L)).thenReturn(Optional.of(savedGame));
+
+        userGameService.favoriteGame(1L, 1L);
+
+        verify(userGameRepository).findByIdAndUserId(1L, 1L);
+        verify(userGameRepository).save(argThat(UserGame::isFavorite));
+        verifyNoMoreInteractions(userGameRepository);
+    }
+
+    @Test
+    void shouldUnfavoriteGameSuccessfully() {
+        UserGame savedGame = new UserGame(1L, "Fallout");
+
+        when(userGameRepository.findByIdAndUserId(1L, 1L)).thenReturn(Optional.of(savedGame));
+        savedGame.favoriteGame();
+
+        userGameService.unfavoriteGame(1L, 1L);
+
+        verify(userGameRepository).findByIdAndUserId(1L, 1L);
+        verify(userGameRepository).save(argThat(game ->
+                !game.isFavorite()));
+        verifyNoMoreInteractions(userGameRepository);
+    }
+
+    @Test
+    void shouldDeleteGameSuccessfully() {
+        UserGame savedGame = new UserGame(1L, "Fallout");
+        when(userGameRepository.findByIdAndUserId(1L, 1L)).thenReturn(Optional.of(savedGame));
+
+        userGameService.deleteGame(1L, 1L);
+
+        verify(userGameRepository).findByIdAndUserId(1L, 1L);
+        verify(userGameRepository).delete(savedGame);
+        verifyNoMoreInteractions(userGameRepository);
+    }
+
+    @Test
+    void shouldReturnConflictWhenGameExistsByUserIdAndIgdbId() {
+        GameSearchResponse response = new GameSearchResponse(
+                1L,
+                "Fallout",
+                LocalDate.now(),
+                List.of("PC"),
+                List.of("Role Playing Game")
+        );
+
+        when(igdbService.searchGameById(1L)).thenReturn(response);
+        when(userGameRepository.existsByUserIdAndIgdbId(1L, 1L)).thenReturn(true);
+
+        assertThrows(GameAlreadyExistsException.class, () -> userGameService.addGame(1L, new AddGameRequest(1L)));
+        verifyNoMoreInteractions(userGameRepository, igdbService);
+        verifyNoInteractions(userRepository);
+    }
+
+    @Test
+    void shouldReturnNotFoundWhenUserNotFound() {
+        GameSearchResponse response = new GameSearchResponse(
+                1L,
+                "Fallout",
+                LocalDate.now(),
+                List.of("PC"),
+                List.of("Role Playing Game")
+        );
+
+        when(igdbService.searchGameById(1L)).thenReturn(response);
+        when(userGameRepository.existsByUserIdAndIgdbId(1L, 1L)).thenReturn(false);
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userGameService.addGame(1L, new AddGameRequest(1L)));
+        verifyNoMoreInteractions(userGameRepository, igdbService, userRepository);
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenGameStatusIsInvalid() {
+        UserGame userGame = new UserGame(
+                1L,
+                "Fallout"
+        );
+        ReflectionTestUtils.setField(userGame, "status", GameStatus.TO_PLAY);
+        when(userGameRepository.findByIdAndUserId(1L, 1L)).thenReturn(Optional.of(userGame));
+
+        assertThrows(InvalidGameStatusException.class, () -> userGameService.updateGameStatus(1L, 1L, new UpdateGameStatusRequest(GameStatus.TO_PLAY)));
+       verifyNoMoreInteractions(userGameRepository);
+    }
 }
--- src/test/java/com/leandroftm/game_library_api/service/UserServiceTest.java
+++ src/test/java/com/leandroftm/game_library_api/service/UserServiceTest.java
@@ -1,4 +1,293 @@
 package com.leandroftm.game_library_api.service;
 
+import com.leandroftm.game_library_api.domain.dto.request.CreateUserRequest;
+import com.leandroftm.game_library_api.domain.dto.request.UpdateUserRequest;
+import com.leandroftm.game_library_api.domain.entity.User;
+import com.leandroftm.game_library_api.exception.domain.user.PasswordConflictException;
+import com.leandroftm.game_library_api.exception.domain.user.UserEmailAlreadyExistsException;
+import com.leandroftm.game_library_api.exception.domain.user.UserNameAlreadyExistsException;
+import com.leandroftm.game_library_api.exception.domain.user.UserNotFoundException;
+import com.leandroftm.game_library_api.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    private final long id = 1L;
+    private final String encodedPassword = "encoded-password";
+
+    @Test
+    void shouldCreateUserSuccessfully() {
+        CreateUserRequest request = new CreateUserRequest(
+                "userName",
+                "password",
+                "email@email.com"
+        );
+
+        when(userRepository.existsByUserNameIgnoreCase(request.userName())).thenReturn(false);
+
+        when(userRepository.existsByEmail(request.email())).thenReturn(false);
+
+        when(passwordEncoder.encode(request.password())).thenReturn(encodedPassword);
+
+        userService.createUser(request);
+
+        verify(userRepository).save(argThat(user ->
+                user.getUserName().equals(request.userName()) &&
+                        user.getPassword().equals(encodedPassword) &&
+                        user.getEmail().equals(request.email())
+        ));
+        verifyNoMoreInteractions(userRepository, passwordEncoder);
+    }
+
+    @SuppressWarnings("")
+//    @Test
+//    void shouldReturnAllUsersSuccessfully() {
+//        Pageable pageable = PageRequest.of(0, 10);
+//        List<User> users = List.of(newUser());
+//
+//        Page<User> userPage = new PageImpl<>(users, pageable, users.size());
+//
+//        when(userRepository.findAll(any(Pageable.class))).thenReturn(userPage);
+//
+//        Page<UserResponse> response = userService.getUsers(pageable);
+//
+//        assertNotNull(response);
+//        assertEquals("userName", response.getContent().getFirst().userName());
+//        assertEquals("email@email.com", response.getContent().getFirst().email());
+//        verifyNoMoreInteractions(userRepository);
+//    }
+
+    @Test
+    void shouldUpdateUserNameSuccessfully() {
+        UpdateUserRequest request = new UpdateUserRequest(
+                "newUserName",
+                null,
+                null
+        );
+
+        User savedUser = newUser();
+        ReflectionTestUtils.setField(savedUser, "id", id);
+
+
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(savedUser));
+        when(userRepository.existsByUserNameIgnoreCaseAndIdNot(request.userName(), id)).thenReturn(false);
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+
+        userService.updateUser(id, request);
+
+        verify(userRepository).findById(id);
+        verify(userRepository).existsByUserNameIgnoreCaseAndIdNot(request.userName(), id);
+        verify(userRepository).save(argThat(user ->
+                user.getUserName().equals(request.userName())));
+        verifyNoMoreInteractions(userRepository);
+        verifyNoInteractions(passwordEncoder);
+    }
+
+    @Test
+    void shouldUpdateEmailSuccessfully() {
+        UpdateUserRequest request = new UpdateUserRequest(
+                null,
+                null,
+                "new.email@email.com"
+        );
+
+        User savedUser = newUser();
+        ReflectionTestUtils.setField(savedUser, "id", id);
+
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(savedUser));
+        when(userRepository.existsByEmailAndIdNot(request.email(), id)).thenReturn(false);
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+
+        userService.updateUser(id, request);
+
+        verify(userRepository).findById(id);
+        verify(userRepository).existsByEmailAndIdNot(request.email(), id);
+        verify(userRepository).save(argThat(user ->
+                user.getEmail().equals(request.email())));
+        verifyNoMoreInteractions(userRepository);
+        verifyNoInteractions(passwordEncoder);
+    }
+
+    @Test
+    void shouldUpdatePasswordSuccessfully() {
+        UpdateUserRequest request = new UpdateUserRequest(
+                null,
+                "new.password",
+                null
+        );
+        User savedUser = newUser();
+        ReflectionTestUtils.setField(savedUser, "id", id);
+
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(savedUser));
+        when(passwordEncoder.encode(request.password())).thenReturn("new-" + encodedPassword);
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+
+        userService.updateUser(id, request);
+
+        verify(userRepository).findById(id);
+        verify(userRepository).save(argThat(user ->
+                user.getPassword().equals("new-encoded-password")));
+        verifyNoMoreInteractions(userRepository, passwordEncoder);
+    }
+
+    @Test
+    void shouldEnableUserSuccessfully() {
+        User savedUser = newUser();
+        ReflectionTestUtils.setField(savedUser, "id", id);
+        savedUser.disable();
+
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(savedUser));
+        assertFalse(savedUser.isActive());
+
+        userService.enable(id);
+        assertTrue(savedUser.isActive());
+
+        verify(userRepository).findById(id);
+        verify(userRepository).save(argThat(User::isActive));
+        verifyNoMoreInteractions(userRepository);
+        verifyNoInteractions(passwordEncoder);
+    }
+
+    @Test
+    void shouldDisableUserSuccessfully() {
+        User savedUser = newUser();
+        ReflectionTestUtils.setField(savedUser, "id", id);
+
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(savedUser));
+        assertTrue(savedUser.isActive());
+
+        userService.disable(id);
+        assertFalse(savedUser.isActive());
+
+        verify(userRepository).findById(id);
+        verify(userRepository).save(argThat(user ->
+                !user.isActive()));
+        verifyNoMoreInteractions(userRepository);
+        verifyNoInteractions(passwordEncoder);
+    }
+
+    @Test
+    void shouldReturnConflictWhenUserNameAlreadyExists() {
+        CreateUserRequest request = new CreateUserRequest(
+                "userName",
+                "password",
+                "email@email.com"
+        );
+
+        when(userRepository.existsByUserNameIgnoreCase(request.userName())).thenReturn(true);
+
+        assertThrows(UserNameAlreadyExistsException.class, () -> userService.createUser(request));
+        verifyNoMoreInteractions(userRepository);
+        verifyNoInteractions(passwordEncoder);
+    }
+
+    @Test
+    void shouldReturnConflictWhenEmailAlreadyExists() {
+        CreateUserRequest request = new CreateUserRequest(
+                "userName",
+                "password",
+                "email@email.com"
+        );
+
+        when(userRepository.existsByUserNameIgnoreCase(request.userName())).thenReturn(false);
+        when(userRepository.existsByEmail(request.email())).thenReturn(true);
+
+        assertThrows(UserEmailAlreadyExistsException.class, () -> userService.createUser(request));
+        verifyNoMoreInteractions(userRepository);
+        verifyNoInteractions(passwordEncoder);
+    }
+
+    @Test
+    void shouldReturnNotFoundWhenUserNotFound() {
+        UpdateUserRequest request = new UpdateUserRequest(
+                "new userName",
+                null,
+                null
+        );
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userService.updateUser(id, request));
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    void shouldReturnConflictWhenUserNameAlreadyExistsOnAnotherUser() {
+        UpdateUserRequest request = new UpdateUserRequest(
+                "new userName",
+                null,
+                null
+        );
+
+        User savedUser = newUser();
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(savedUser));
+        when(userRepository.existsByUserNameIgnoreCaseAndIdNot(request.userName(), id)).thenReturn(true);
+
+        assertThrows(UserNameAlreadyExistsException.class, () -> userService.updateUser(id, request));
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    void shouldReturnConflictWhenEmailAlreadyExistsOnAnotherUser() {
+        UpdateUserRequest request = new UpdateUserRequest(
+                null,
+                null,
+                "new.email@email.com"
+        );
+
+        User savedUser = newUser();
+
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(savedUser));
+        when(userRepository.existsByEmailAndIdNot(request.email(), id)).thenReturn(true);
+
+        assertThrows(UserEmailAlreadyExistsException.class, () -> userService.updateUser(id, request));
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    void shouldReturnConflictWhenNewAndOldPasswordAreIdentical() {
+        UpdateUserRequest request = new UpdateUserRequest(
+                null,
+                "password",
+                null
+        );
+
+        User savedUser = newUser();
+
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(savedUser));
+
+      when(passwordEncoder.matches(request.password(), savedUser.getPassword())).thenReturn(true);
+      assertThrows(PasswordConflictException.class, () -> userService.updateUser(id, request));
+      verifyNoMoreInteractions(userRepository);
+    }
+
+    private User newUser() {
+        return new User(
+                "userName",
+                "password",
+                "email@email.com"
+        );
+    }
 }
